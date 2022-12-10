e2eeec0a69402c20f11d7fe6a9e8ea24d21e8fe8
Diego Valenzuela <dazet@outlook.cl>
2022-12-10 17:06:38 -0300

    [+] install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,14 @@ module.exports = {
   content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{ts,tsx}",
+      "./public/**/*.html",
+      "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require("flowbite/plugin")
+  ],
 }
